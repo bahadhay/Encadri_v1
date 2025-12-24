1e7f51a70b62c3ffc884bd54a4f749e5b2d28749
baha dhay <BAHA.DHAY@EPISOUSSE.COM.TN>
2025-12-24 22:25:40 +0100

    Update Railway URL

--- Encadri-Frontend/src/environments/environment.prod.ts
+++ Encadri-Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://encadri-backend-production.up.railway.app/api',
-  hubUrl: 'https://encadri-backend-production.up.railway.app/hubs'
+  apiUrl: 'https://encadri-v1-production.up.railway.app/api',
+  hubUrl: 'https://encadri-v1-production.up.railway.app/hubs'
 };
